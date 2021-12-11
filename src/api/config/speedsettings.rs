6ba0d0a7d8db352ff07e7f6a3f6cba3225f56a0c
Josh Holmer <jholmer.in@gmail.com>
2021-12-10 19:37:43 -0500

    16x16 test

--- src/api/config/speedsettings.rs
+++ src/api/config/speedsettings.rs
@@ -154,7 +154,7 @@ impl SpeedSettings {
     }
 
     if speed >= 2 {
-      settings.non_square_partition_threshold = BlockSize::BLOCK_8X8;
+      settings.non_square_partition_threshold = BlockSize::BLOCK_16X16;
     }
 
     if speed >= 3 {
